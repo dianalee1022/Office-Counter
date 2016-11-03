eb90f7452301ae3437be785692414b3e62c8e690
dianalee1022 <dianalee1022@users.noreply.github.com>
2016-11-03 16:03:37 -0400

    The initial Arduino code for testing purpose
    Before counters are added

---
+++ pushButton/pushButton.ino
@@ -0,0 +1,23 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600); 
+    buttonState = digitalRead(buttonPin);
+
+
+  if (buttonState == HIGH) {
+    Serial.print("Pressed");
+  } else {
+    Serial.print("Off");
+  }
+  delay(20);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
