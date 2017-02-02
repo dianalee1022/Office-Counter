6129e389317e262c74222afc282fe8e10533f6bd
Diana Lee <diana.lee5026@gmail.com>
2017-02-02 10:29:26 -0500

    Date Object added
    Date object added to track the current date

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Title: Office Counter
 
 import kivy
 kivy.require('1.0.6')
+import time
+import datetime
 
 from kivy.app import App
 from kivy.uix.button import Button
@@ -14,21 +16,27 @@ from kivy.uix.label import Label
 from kivy.properties import *
 from kivy.graphics import Color, Rectangle, Point, GraphicException
 
+now = datetime.datetime.now()
+currentTime = str(now.day) + "/" + str(now.month) + "/" + str(now.year)
+
 class Boxes(FloatLayout):
-    main_container = ObjectProperty(None)
-    def call_pressed(self):
-        self.ids.call_count.text = "hello it's call!"
-    def inperson_pressed(self):
-        self.ids.inperson_count.text = "hellow it's in person!"
-    def call_released(self):
-        self.ids.call_count.text = "number of calls!"
-    def inperson_released(self):
-        self.ids.inperson_count.text = "number of inperson counts!"
+	main_container = ObjectProperty(None)
+	
+	def call_pressed(self):
+		global currentTime
+		result = "hello it's call!" + currentTime
+		self.ids.call_count.text = result
+	def inperson_pressed(self):
+		self.ids.inperson_count.text = "hellow it's in person!"
+	def call_released(self):
+		self.ids.call_count.text = "number of calls!"
+	def inperson_released(self):
+		self.ids.inperson_count.text = "number of inperson counts!"
 
 
 class CounterApp(App):
-    def build(self):
-        return Boxes()
+	def build(self):
+		return Boxes()
 
 if __name__ == '__main__':
     CounterApp().run()
